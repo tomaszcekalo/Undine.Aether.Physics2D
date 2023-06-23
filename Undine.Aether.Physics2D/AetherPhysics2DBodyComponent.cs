b0fede3be52eddee708d2fdd230d24f16b45acc9
Tomasz Cekało <tomasz.cekalo@gmail.com>
2023-06-23 09:31:35 +0200

    Add project files.

---
+++ Undine.Aether.Physics2D/AetherPhysics2DBodyComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tainicom.Aether.Physics2D.Dynamics;
+
+namespace Undine.Aether.Physics2D
+{
+    public struct AetherPhysics2DBodyComponent
+    {
+        public Body Body;
+    }
+}
---
+++ Undine.Aether.Physics2D/AetherPhysics2DSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Undine.Core;
+
+namespace Undine.Aether.Physics2D
+{
+    internal class AetherPhysics2DSystem : UnifiedSystem<AetherPhysics2DWorldComponent>
+    {
+        public float ElapsedGameTimeTotalSeconds { get; set; }
+
+        public override void ProcessSingleEntity(int entityId, ref AetherPhysics2DWorldComponent t)
+        {
+            t.World.Step(ElapsedGameTimeTotalSeconds);
+        }
+    }
+}
